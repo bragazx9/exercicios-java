c171b00f8c348e4e5c9759e65cc8267e0798edcf
Bernardo Braga <bragabernardo454@gmail.com>
2025-01-30 21:46:31 -0300

    atualizando

---
+++ src/parte5/EX2.java
@@ -0,0 +1,21 @@
+package parte5;
+
+import java.util.Scanner;
+
+public class EX2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um mes em numero");
+        int mes = sc.nextInt();
+
+        if (mes > 12 && mes > 0)
+        {
+            System.out.println("Mes invalido");
+        }
+        else {
+            System.out.println(" mes valido");
+        }
+            sc.close();
+    }
+}
