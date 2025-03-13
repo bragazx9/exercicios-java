b0a6c049f9aa2a51e75a95b75044064ce493d060
bragazx9 <bragabernardo454@gmail.com>
2025-03-13 18:27:48 -0300

    Update Main.java

--- gettersAndSetters/Main.java
+++ gettersAndSetters/Main.java
@@ -2,29 +2,27 @@ package gettersAndSetters;
 
 public class Main {
     public static void main(String[] args) {
-        // Teste da classe Pessoa
+
         Pessoa pessoa = new Pessoa();
-        pessoa.setNome("João");
-        pessoa.setIdade(25);
+        pessoa.setNome("Bernardin");
+        pessoa.setIdade(17);
         System.out.println("Nome: " + pessoa.getNome());
         System.out.println("Idade: " + pessoa.getIdade());
 
-        // Teste da classe ContaBancaria
-        ContaBancaria conta = new ContaBancaria("Maria", 1000);
+        ContaBancaria conta = new ContaBancaria("FpBala", 10.000);
         System.out.println("Titular: " + conta.getTitular());
         System.out.println("Saldo inicial: R$" + conta.getSaldo());
         conta.depositar(500);
         conta.sacar(300);
         System.out.println("Saldo atual: R$" + conta.getSaldo());
 
-        // Teste da classe Produto
-        Produto produto = new Produto("Celular", 1500, 10);
+        Produto produto = new Produto("COmputador", 1500, 10);
         System.out.println("Produto: " + produto.getNome());
         System.out.println("Preço: R$" + produto.getPreco());
-        System.out.println("Estoque: " + produto.getEstoque() + " unidades");
-        produto.adicionarEstoque(5);
-        produto.removerEstoque(8);
-        System.out.println("Estoque final: " + produto.getEstoque() + " unidades");
+        System.out.println("Estoque: " + produto.getEstoque() + " unidade");
+        produto.adicionarEstoque(6);
+        produto.removerEstoque(10);
+        System.out.println("Estoque final: " + produto.getEstoque() + " unidade");
     }
 }
 
